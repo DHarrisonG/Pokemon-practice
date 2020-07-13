0a3a6e0cf936ce5433e6b34e07453b84160e1c57
github username <github email address>
2020-07-13 17:15:33 -0500

    reverting to original lab

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,39 +1,27 @@
-import React from 'react';
-import { Card } from 'semantic-ui-react';
+import React from 'react'
+import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      flipped: false,
-    };
-  }
-
   render() {
-    const poke = this.props.pokemon;
-
     return (
       <Card>
-        <div onClick={() => this.setState({ flipped: !this.state.flipped })}>
+        <div>
           <div className="image">
-            <img
-              alt="oh no!"
-              src={this.state.flipped ? poke.sprites.back : poke.sprites.front}
-            />
+            <img alt="oh no!" />
           </div>
           <div className="content">
-            <div className="header">{poke.name}</div>
+            <div className="header">POKEMON NAME HERE</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              {poke.stats.find((stat) => stat.name === 'hp').value} hp
+              POKEMON HP HERE hp
             </span>
           </div>
         </div>
       </Card>
-    );
+    )
   }
 }
 
-export default PokemonCard;
+export default PokemonCard
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,20 +1,15 @@
-import React from 'react';
-import PokemonCard from './PokemonCard';
-import { Card } from 'semantic-ui-react';
+import React from 'react'
+import PokemonCard from './PokemonCard'
+import { Card } from 'semantic-ui-react'
 
 class PokemonCollection extends React.Component {
   render() {
     return (
-      <div>
+      <Card.Group itemsPerRow={6}>
         <h1>Hello From Pokemon Collection</h1>
-        <Card.Group itemsPerRow={6}>
-          {this.props.pokemon.map((poke) => (
-            <PokemonCard key={poke.id}  pokemon={poke}></PokemonCard>
-          ))}
-        </Card.Group>
-      </div>
-    );
+      </Card.Group>
+    )
   }
 }
 
-export default PokemonCollection;
+export default PokemonCollection
